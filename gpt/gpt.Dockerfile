524c8781fe6d658747ee426abe06c4a1a4c3382b
lsc713 <lsc7134@naver.com>
2024-12-07 10:47:02 +0900

    [KAN-268] chore 폴더 구조 변경

--- gpt/gpt.Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.8-slim
-
-COPY gpt-requirements.txt gpt-requirements.txt
-COPY csv-to-gpt.py csv-to-gpt.py
-
-RUN pip install -r gpt-requirements.txt
-
-ENTRYPOINT ["python3", "csv-to-gpt.py"]
--- mysql-batch/create_table.py
+++
@@ -1,9 +0,0 @@
-from delete_table_list import *
-
-def create_table(cursor):
-    
-    cursor.execute(delete_table_restaurants)
-    cursor.execute(delete_table_categories)
-    cursor.execute(delete_table_restaurant_categories)
-    cursor.execute(delete_table_menus)
-    
